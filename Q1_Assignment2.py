9144fb4e2f6e4b222651daaed9264357ca0a7fd7
igormusic <igormusich@gmail.com>
2024-05-30 00:13:00 -0400

    Suggested changes

--- Q1_Assignment2.py
+++ Q1_Assignment2.py
@@ -1,69 +1,60 @@
 # Improved Guessing Game
-#Potential changes- separate min and max input functions so can give feedback immediately
-#ask if should be able to quit during range setting
+# Potential changes - separate min and max input functions so can give feedback immediately
+# ask if should be able to quit during range setting
 
 # Marina Music (20496250)
 # May 27th, 2024
-
-
-
 
 
 # Encodes a random number generator that generates a random
 # number between 1 and 10 (inclusive) and assigns that number
 # to the variable winning_number.
 
-import random                                                    #https://shorturl.at/9uS7J
+import random  # https://shorturl.at/9uS7J
+from typing import Union
 
-def getMinMax():
+
+def get_min_max() -> (str, str):
     min_input = input("Please enter a positive, non-zero integer value to set as the guessing range minimum:\n")
     max_input = input("Please enter a positive, non-zero integer value to set as the guessing range maximum:\n")
     return min_input, max_input
 
-def checkMinMax(minimum, maximum):
-    if minimum.isnumeric() and maximum.isnumeric():
-        if int(maximum) > int(minimum) > 0:
-            return 'valid'
-        else:
-            return 'invalid'
-    else:
-        return 'invalid'
+
+def validate_min_max(minimum: str, maximum: str) -> bool:
+    return minimum.isnumeric() and maximum.isnumeric() and int(maximum) > int(minimum) > 0
 
 
-
-def generateRandomNumber(minimum, maximum):
-    winning_number = random.randrange(minimum, maximum)
-    return winning_number
-
-def getGuess(minimum,maximum):
-    guess = input(f"Enter an integer number between {minimum} and {maximum} or to give up type \'quit\':\n")
-    return guess
+def generate_random_number(minimum, maximum):
+    return random.randrange(minimum, maximum)
 
 
-def checkGuess(guess, maximum, minimum):
-    if guess.isnumeric():
-        if maximum >= int(guess) >= minimum:
-            return int(guess)
+def get_guess(minimum: int, maximum: int) -> str:
+    return input(f"Enter an integer number between {minimum} and {maximum} or to give up type \'quit\':\n")
+
+
+def check_guess(guess: str, maximum: int, minimum: int) -> Union[str, int]:
+    if guess.isnumeric() and maximum >= int(guess) >= minimum:
+        return int(guess)
     elif guess.casefold() == 'quit':
         return 'quit'
     else:
         return 'invalid'
 
-def compareNumbers(guess, winning_number):
+
+def is_valid_answer(guess: str, winning_number: int) -> bool:
     if guess == 'quit':
         print(f"\nThe correct answer was {winning_number}. Thanks for letting me win, goodbye!")
-        return 'quit'
+        return True
     else:
         if guess == winning_number:
             print(f"You win! The correct answer was {winning_number}!")
-            return 'right'
-        elif guess > winning_number:
+            return True
+        elif int(guess) > winning_number:
             print("Wrong! Your guess is too high. Try again.\n")
-            return 'wrong'
+            return False
         else:
             print("Wrong! Your guess is too low. Try again.\n")
-            return 'wrong'
-
+            return False
 
 
 def main():
@@ -71,36 +62,33 @@ def main():
           "You will enter a minimum and maximum value and I will think of a number in that range.\n"
           "Then you will guess the number I'm thinking of! "
           "Let's begin...\n")
-    min_max = getMinMax()
-    minimum, maximum = min_max              #unpack tuple https://martinxpn.medium.com/what-are-multiple-return-values-actually-in-python-28-100-days-of-python-82821c8de24b#:~:text=In%20Python%2C%20it%20is%20possible,very%20useful%20in%20many%20scenarios.
-    check_min_max = checkMinMax(minimum, maximum)
-    while check_min_max != 'valid':
-        print("Invalid input. Please try again.")
-        min_max = getMinMax()
-        minimum, maximum = min_max
-        check_min_max = checkMinMax(minimum, maximum)
-    minimum = int(minimum)
-    maximum = int(maximum)
-    winning_number = generateRandomNumber(minimum, maximum)
-    guess_input = getGuess(minimum, maximum)
-    validated_guess = checkGuess(guess_input, maximum, minimum)
-    while validated_guess == 'invalid':
-        print("Invalid input. Please try again.\n")
-        guess_input = getGuess(minimum,maximum)
-        validated_guess = checkGuess(guess_input, maximum, minimum)
-    right_or_wrong = compareNumbers(validated_guess, winning_number)
-    while right_or_wrong == 'wrong' and validated_guess != 'quit':
-        guess_input = getGuess(minimum, maximum)
-        validated_guess = checkGuess(guess_input, maximum, minimum)
-        while validated_guess == 'invalid':
-            print("Invalid input. Please try again.\n")
-            guess_input = getGuess(minimum, maximum)
-            validated_guess = checkGuess(guess_input, maximum, minimum)
-        right_or_wrong = compareNumbers(validated_guess, winning_number)
 
+    while True:
+        min_input, max_input = get_min_max()
+        is_valid = validate_min_max(min_input, max_input)
+        if is_valid:
+            break
+        else:
+            print("Invalid input. Please try again.")
 
+    minimum = int(min_input)
+    maximum = int(max_input)
 
+    winning_number = generate_random_number(minimum, maximum)
 
+    while True:
+        while True:
+            guess_input = get_guess(minimum, maximum)
+            validated_guess = check_guess(guess_input, maximum, minimum)
+            if validated_guess != 'invalid':
+                break
+            else:
+                print("Invalid input. Please try again.\n")
+
+        game_over = is_valid_answer(validated_guess, winning_number)
+
+        if game_over:
+            break
 
     """1. Prompt user for min and max value
     - ask for min input
@@ -125,7 +113,8 @@ def main():
             - print("Too high")
             - guess = getGuess()
     6. Win: If guess == winning_number:
-        print("win statment and winning_number")
-    7. giveUp()- If use types "quit" at any point program should print winning""" # and terminate
-main()
+        print("win statement and winning_number")
+    7. giveUp()- If use types "quit" at any point program should print winning"""  # and terminate
 
+
+main()
